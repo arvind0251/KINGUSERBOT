49fbf2a82246ea1ffe42702e08a5c0b95308792c
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2026-08-10 14:24:08 +0530

    Fix: Correct clear() function - don't always raise exception

--- Branded/modules/utilities/queues.py
+++ Branded/modules/utilities/queues.py
@@ -34,11 +34,11 @@ def task_done(chat_id: int):
             pass
 
 
-def clear(chat_id: int):
+async def clear(chat_id: int):
     if chat_id in queues:
         if queues[chat_id].empty():
             raise Empty
         else:
             queues[chat_id].queue = []
+            return
     raise Empty
-    
