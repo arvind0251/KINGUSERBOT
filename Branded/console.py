f4f7a48aad4b857961afee002a9439e7db0ac738
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2026-08-10 12:58:14 +0530

    Remove MONGO_DB_URL from environment variables

--- Branded/console.py
+++ Branded/console.py
@@ -32,7 +32,6 @@ API_ID = int(getenv("API_ID", 0))
 API_HASH = getenv("API_HASH", None)
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING_SESSION = getenv("STRING_SESSION", None)
-MONGO_DB_URL = getenv("MONGO_DB_URL", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
 
 
